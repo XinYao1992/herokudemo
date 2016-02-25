8205e68c3cfd03180923e5cf03770afa6d75548a
Xin Yao <xyao2@hotmail.com>
2016-02-25 01:21:54 -0500

    first demo is done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,16 @@ require './lib/map.rb'
 
 class WelcomeController < ApplicationController
   def index
-    map = CampusMap.new()
-    map.load("./lib/buildings.txt", "./lib/paths.txt")
-    @txt = map.solve(1059, 1060)
+    @notes = [1059, 1060]
+    if params[:start] != nil && params[:end] != nil
+      startP = params[:start].to_i
+      endP = params[:end].to_i
+      map = CampusMap.new()
+      map.load("./lib/buildings.txt", "./lib/paths.txt")
+      @result = map.solve(startP, endP).html_safe
+    end
   end
 
-  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  get 'welcome/index'
   #get 'member/:id' => 'members#show'
 
   root 'welcome#index'
