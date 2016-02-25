1d6794f8cd7768c181be660411f46a39609d2f83
Xin Yao <xyao2@hotmail.com>
2016-02-24 22:29:12 -0500

    require map.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
+require './lib/map.rb'
+
 class WelcomeController < ApplicationController
   def index
+    map = CampusMap.new()
+    map.load("./lib/buildings.txt", "./lib/paths.txt")
+    @txt = map.solve(1059, 1060)
   end
+
+  
+
 end
